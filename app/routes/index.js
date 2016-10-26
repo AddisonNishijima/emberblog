0fbf7c99f26b2b75f61ec0e23beb07ee13466092
Addison Nishijima <addison.nishijima@gmail.com>
2016-10-26 10:15:15 -0700

    add model data to index.js

---
+++ app/routes/index.js
@@ -0,0 +1,42 @@
+import Ember from 'ember';
+
+var posts = [(
+  id: 1,
+  day: "Wednesday",
+  topic: "Tokay Gecko",
+  sciname: "Gekko gekko",
+  image: "http://rivista-cdn.reptilesmagazine.com/normal-tokay-gecko.jpg?ver=1410818182",
+  description: "Tokay Geckos are from Malaysia and are common in the pet trade. They are, despite their smiling appearances, not friendly."
+}, {
+  id: 2,
+  day: "Tuesday",
+  topic: "Leopard Gecko",
+  sciname: "Eublepharis macularius",
+  image: "http://rivista-cdn.reptilesmagazine.com/images/cache/cache_b/cache_b/cache_8/6211-leopardgeckosleopard-gecko001-9afa38bb.jpeg?ver=1468546044&aspectratio=1.4978260869565",
+  description: "Leopard Geckos originated in the Middle East and are one of the most common reptiles in the pet trade. They make wonderful beginner pets and tolerate handling well."
+}, {
+  id: 3,
+  day: "Monday",
+  topic: "Giant Day Gecko",
+  sciname: "Phelsuma grandis",
+  image: "http://4.bp.blogspot.com/-KRuQBLqNzCc/UUdZMRx9ePI/AAAAAAAAET4/0vG01fQK3Ug/s1600/phelsuma_madagascariensis_grandis_01.jpg",
+  description: "Giant Day Geckos originated in Madagascar. They are becoming increasingly common in the pet trade. They have much more complex care than other geckos, and their fragile skin makes them a pet that is pretty to look at, but not good to handle."
+}, {
+  id: 4,
+  day: "Sunday",
+  topic: "Crested Gecko",
+  sciname: "Correlophus ciliatus",
+  image: "http://40.media.tumblr.com/ccaca67681b66e9beb8042ac0b59be74/tumblr_mzw3gdMUxy1rdag7yo7_1280.jpg",
+  description: "The Crested Gecko is another very common gecko in the pet trade. They have very simple care and despite being quite active (and fast) can make very handleable pets."
+}, {
+  id: 5,
+  day: "Saturday",
+  topic: "Introduction",
+  sciname: "Blahdeblah",
+  image: "http://s2.quickmeme.com/img/d6/d6a1143f571184db25f94613edd43b40af6d3a629221aba00d9efdcfef5efd84.jpg",
+  description: "Here is my first post. I will be posting geckos on this blog. This is so informative. Yay."
+}
+)]
+
+export default Ember.Route.extend({
+});
