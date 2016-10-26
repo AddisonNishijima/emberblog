49659744e043115a9e4a4f29aff287e5d5e17224
Addison Nishijima <addison.nishijima@gmail.com>
2016-10-26 13:52:11 -0700

    fix error with update component

--- app/components/update-post.js
+++ app/components/update-post.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updatePostForm: false,
+  actions: {
+    updatePostForm() {
+      this.set('updatePostForm', true);
+    },
+
+    update(post) {
+      var params = {
+        day: this.get('day'),
+        topic: this.get('topic'),
+        sciname: this.get('sciname'),
+        image: this.get('image'),
+        description: this.get('description'),
+      };
+      this.set('updatePostForm', false);
+      this.sendAction('update', post, params);
+    }
+  }
 });
