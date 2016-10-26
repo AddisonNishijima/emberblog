baaf06ac7e14eac1c22c2916ff021e98c8158a70
Addison Nishijima <addison.nishijima@gmail.com>
2016-10-26 14:50:57 -0700

    attempt to fix details not showing

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,28 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('post');
-  },
-
-  actions: {
-    savePost3(params) {
+     return this.store.findAll('post');
+   },
+   actions: {
+    saveRental3(params) {
       var newPost = this.store.createRecord('post', params);
       newPost.save();
-      this.transitionTo('index');
-    },
-
-    update(post, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          post.set(key,params[key]);
-        }
-      });
-      post.save();
-      this.transitionTo('index');
-    },
-
-    destroyPost(post) {
-      post.destroyRecord();
       this.transitionTo('index');
     }
   }
